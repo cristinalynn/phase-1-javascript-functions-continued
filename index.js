523eecad2eef5a18c76d3a7cd0071ac1afb5cda3
Cristina Lynn <cristina@cristinalynn.net>
2023-01-14 17:44:15 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(dog = "roller-skate"){
+
+    return (`This Saturday, I want to ${dog}!`);
+}
+const mondayWork = function(cat = "go to the office"){
+    return (`This Monday, I will ${cat}.`);
+}
+
+function wrapAdjective(flair = "*"){
+    return function innerFunction(bird = "special"){
+        return `You are ${flair}${bird}${flair}!`
+    }
+
+}
